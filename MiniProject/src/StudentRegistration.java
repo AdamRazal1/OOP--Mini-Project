9d690704ef667f9dd30432a543ed7bf99468fd9a
fiqdanish <afiq.dnish121@gmail.com>
2025-06-18 23:31:08 +0800

    Create folder structure and main file

---
+++ MiniProject/src/StudentRegistration.java
@@ -0,0 +1,22 @@
+class User{
+
+}
+
+class Admin extends User{
+
+}
+
+class Student extends User{
+
+}
+
+class Course{
+
+}
+
+
+public class StudentRegistration {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+}
